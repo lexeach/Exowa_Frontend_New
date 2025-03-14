74b4defa5ef7b4d88e11fd57cb87b53a4366a835
lexeach <80155340+lexeach@users.noreply.github.com>
2025-03-14 16:22:29 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,6 @@
 // export const apiUrl = "http://localhost:4000"; // locals
 // export const BaseURL = 'http://localhost:5173' // locals
-export const BaseURL = 'https://auto-paper.netlify.app'
-export const apiUrl = 'https://ai-exam-be-001.onrender.com' 
+export const BaseURL = 'https://exowademo.netlify.app'
+//export const apiUrl = 'https://ai-exam-be-001.onrender.com'
+export const apiUrl = 'https://exowa-backend.onrender.com'
+
