b9a1ae8c0ad6a687984ba1451b08566839161c93
lexeach <80155340+lexeach@users.noreply.github.com>
2025-03-15 20:34:58 +0530

    Update config.ts

--- src/Pages/Paper/config.ts
+++ src/Pages/Paper/config.ts
@@ -2,31 +2,31 @@ import * as yup from "yup";
 
 const options = [
   {
-    value: 1,
+    value: 5,
     label: "1",
   },
   {
-    value: 2,
+    value: 10,
     label: "2",
   },
   {
-    value: 3,
+    value: 15,
     label: "3",
   },
   {
-    value: 4,
+    value: 20,
     label: "4",
   },
   {
-    value: 5,
+    value: 25,
     label: "5",
   },
   {
-    value: 6,
+    value: 30,
     label: "6",
   },
   {
-    value: 7,
+    value: 35,
     label: "7",
   },
   {
