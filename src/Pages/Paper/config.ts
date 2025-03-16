d79f2c0636d8420924824a622772a97f1129d90b
lexeach <80155340+lexeach@users.noreply.github.com>
2025-03-16 13:39:56 +0530

    Update config.ts

--- src/Pages/Paper/config.ts
+++ src/Pages/Paper/config.ts
@@ -74,6 +74,57 @@ const classoptions = [
   }, 
 ];
 
+const chapteroptions = [
+  {
+    value: 1,
+    label: "1",
+  },
+  {
+    value: 2,
+    label: "2",
+  },
+  {
+    value: 3,
+    label: "3",
+  },
+  {
+    value: 4,
+    label: "4",
+  },
+  {
+    value: 5,
+    label: "5",
+  },
+  {
+    value: 6,
+    label: "6",
+  },
+  {
+    value: 7,
+    label: "7",
+  }, 
+  {
+    value: 8,
+    label: "8",
+  },
+  {
+    value: 9,
+    label: "9",
+  },
+  {
+    value: 10,
+    label: "10",
+  },
+  {
+    value: 11,
+    label: "11",
+  },
+  {
+    value: 12,
+    label: "12",
+  },
+];
+
 export const fields = (
   useGetSubjectOptionsMutation,
   useGetSyllabusOptionsMutation
@@ -110,7 +161,7 @@ export const fields = (
     label: "Chapter From",
     placeholder: "Select Chapter ...",
     type: "select",
-    options: options,
+    options: chapteroptions,
     wrapperClassName: "mb-6",
     fieldWrapperClassName: "col-span-6",
   },
@@ -119,7 +170,7 @@ export const fields = (
     label: "Chapter To",
     placeholder: "Select Chapter ...",
     type: "select",
-    options: options,
+    options: chapteroptions,
     wrapperClassName: "mb-6",
     fieldWrapperClassName: "col-span-6",
   },
