8291fbe629bb606ac7a6105902b6f1ef6172a584
lexeach <toaddenterprises@gmail.com>
2025-06-13 15:56:35 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 // export const apiUrl = "http://localhost:4000"; // locals
 // export const BaseURL = 'http://localhost:5173' // locals
-export const BaseURL = 'https://user.exowa.click'
+export const BaseURL = 'https://test.exowa.click'
 //export const apiUrl = 'https://ai-exam-be-001.onrender.com'
 export const apiUrl = 'https://exowa-backend.onrender.com'
 
