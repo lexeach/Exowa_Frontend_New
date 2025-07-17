1ac737994799ac18c7f28a6a16b3a020292d167d
lexeach <80155340+lexeach@users.noreply.github.com>
2025-07-17 18:07:37 +0530

    Update config.ts

--- src/Pages/Paper/config.ts
+++ src/Pages/Paper/config.ts
@@ -5,12 +5,12 @@ const options = [
   { value: 10, label: "10" },
   { value: 15, label: "15" },
   { value: 20, label: "20" },
-  { value: 25, label: "25" },
-  { value: 30, label: "30" },
-  { value: 35, label: "35" },
-  { value: 40, label: "40" },
-  { value: 45, label: "45" },
-  { value: 50, label: "50" },
+  //{ value: 25, label: "25" },
+  //{ value: 30, label: "30" },
+  //{ value: 35, label: "35" },
+  //{ value: 40, label: "40" },
+  //{ value: 45, label: "45" },
+  //{ value: 50, label: "50" },
 ];
 
 const classoptions = [
