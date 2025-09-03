b2a9a450930be36f378ed1af86395c5e1ec6b852
lexeach <80155340+lexeach@users.noreply.github.com>
2025-09-03 07:25:10 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 //export const apiUrl = "http://localhost:4001"; // locals URL
-/export const BaseURL = 'http://localhost:5173' // locals URL
+//export const BaseURL = 'http://localhost:5173' // locals URL
  export const BaseURL = 'https://test.exowa.click' // live URL
  export const apiUrl = 'https://exowa-backend.onrender.com' // live URL
 
