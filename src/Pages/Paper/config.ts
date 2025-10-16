ba255a812958a2fb2bb4880bc534d0869c2fb0be
lexeach <80155340+lexeach@users.noreply.github.com>
2025-10-16 13:23:02 +0530

    Update config.ts

--- src/Pages/Paper/config.ts
+++ src/Pages/Paper/config.ts
@@ -8,13 +8,13 @@ const options = [
 ];
 
 const classoptions = [
-  { value: 6, label: "6" },
-  { value: 7, label: "7" },
-  { value: 8, label: "8" },
-  { value: 9, label: "9" },
+  //{ value: 6, label: "6" },
+  //{ value: 7, label: "7" },
+  //{ value: 8, label: "8" },
+  //{ value: 9, label: "9" },
   { value: 10, label: "10" },
-  { value: 11, label: "11" },
-  { value: 12, label: "12" },
+  //{ value: 11, label: "11" },
+  //{ value: 12, label: "12" },
 ];
 
 // Define the dynamic subject options based on class
