21b930dfc75e57905e913f9bea4ff1d2f356bd54
lexeach <80155340+lexeach@users.noreply.github.com>
2025-11-13 10:09:11 +0530

    Update BaseURL and apiUrl for live environment

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,8 @@
 // export const apiUrl = import.meta.env.VITE_API_URL;
 // export const apiUrl = "http://localhost:4000"; // locals
 // export const BaseURL = 'http://localhost:5173' // locals
-export const BaseURL = 'https://auto-paper.netlify.app'
-export const apiUrl = 'https://ai-exam-be-001.onrender.com' 
+//export const BaseURL = 'https://auto-paper.netlify.app'
+//export const apiUrl = 'https://ai-exam-be-001.onrender.com' 
+  export const BaseURL = 'https://test.exowa.click' // live URL
+  export const apiUrl = 'https://exowa-backend.onrender.com' // live URL
+
