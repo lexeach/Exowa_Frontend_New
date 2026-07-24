ba01a553c024068780c09e32141052d16dcfd82c
lexeach <80155340+lexeach@users.noreply.github.com>
2026-07-24 19:59:15 +0530

    Update learningVerification.ts

--- src/service/learningVerification.ts
+++ src/service/learningVerification.ts
@@ -59,4 +59,5 @@ export const {
   useGenerateLearningVerificationMutation,
   useSubmitLearningVerificationMutation,
   useGetLearningVerificationQuery,
+  useLazyGetLearningVerificationQuery,
 } = learningVerificationApi;
