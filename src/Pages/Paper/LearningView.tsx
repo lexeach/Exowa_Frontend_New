dcb72edbe2459b159f89565f5db842662c49aa0a
lexeach <80155340+lexeach@users.noreply.github.com>
2026-08-02 16:18:59 +0530

    Update LearningView.tsx

--- src/Pages/Paper/LearningView.tsx
+++ src/Pages/Paper/LearningView.tsx
@@ -967,7 +967,7 @@ learningData?.data ? (
         src={`https://www.youtube.com/embed/${selectedVideo}?autoplay=1&playsinline=1&fs=1&rel=0&modestbranding=1`}
         title="Learning Video"
         className="absolute inset-0 w-full h-full"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
     />
 
