29f3f91b6eec739718695f5c21dc6a2cf0b55945
lexeach <80155340+lexeach@users.noreply.github.com>
2026-08-03 15:38:21 +0530

    Update LearningVideoPlayer.tsx

--- src/components/Learning/LearningVideoPlayer.tsx
+++ src/components/Learning/LearningVideoPlayer.tsx
@@ -82,8 +82,9 @@ export default function LearningVideoPlayer({
 
                     key={selectedVideo}
 
-                    src={`https://www.youtube-nocookie.com/embed/${selectedVideo}?autoplay=1&playsinline=1&controls=1&rel=0&modestbranding=1`}
+                   // src={`https://www.youtube-nocookie.com/embed/${selectedVideo}?autoplay=1&playsinline=1&controls=1&rel=0&modestbranding=1`}
 
+                    src={`https://www.youtube-nocookie.com/embed/${selectedVideo}?autoplay=1&playsinline=1&controls=1&fs=1&playsinline=0&rel=0&modestbranding=1`}
                     title="Learning Video"
 
                     className="absolute inset-0 w-full h-full"
